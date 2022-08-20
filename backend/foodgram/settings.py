1ba2ea76fe91b66d8c097cd9d2e5f3dae7f5400f
Mikhail Soldatkin <michael.soldatkin@gmail.com>
2022-08-20 16:47:59 +0300

    foodgram workflow day 2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -11,7 +11,8 @@ SECRET_KEY = os.getenv(key='SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.7.21', '127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['158.160.7.21', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
