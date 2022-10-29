234671f1e9643be256f13e482a72abb154082ac7
Mikhail Soldatkin <michael.soldatkin@gmail.com>
2022-10-29 11:05:59 +0300

    isort imports

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,5 @@
+from api.pagination import CustomPagination
+from api.serializers import CustomUserSerializer, SubscribeSerializer
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
@@ -5,9 +7,6 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
-from api.pagination import CustomPagination
-from api.serializers import CustomUserSerializer, SubscribeSerializer
 
 from .models import Subscribe
 
